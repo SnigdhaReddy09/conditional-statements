c3a638ad49b851a288ed6a3449592b0db0dc4bfe
SnigdhaReddy09 <93863995+SnigdhaReddy09@users.noreply.github.com>
2024-10-22 09:34:06 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,33 @@
+// 1
+var age=22
+if(age>18){
+    console.log("eligible for vote")
+}else{
+    console.log("not eligible")
+}
+
+// 2
+var WeatherCondition="rainy"
+if(WeatherCondition){
+    console.log("its pouring outside")
+}else{
+    console.log("not raining")
+}
+
+// 3
+var marks=76
+if(marks>90){
+    console.log("A+")
+}else if(marks>80 && marks<=90){
+    console.log("A")
+}else if(marks>70 && marks<=80){
+    console.log("B+")
+}else if(marks>60 && marks<=70){
+    console.log("C+")
+}else if(marks>50 && marks<=60){
+    console.log("C")
+}else if(marks>=40 && marks<=50){
+    console.log("D")
+}else{
+    console.log("fail")
+}
